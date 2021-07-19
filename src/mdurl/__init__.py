57cb9e1fb00c159bc7fd8c89af1914bee0366420
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2021-07-19 11:57:41 +0300

    Implement encode.js

--- src/mdurl/__init__.py
+++ src/mdurl/__init__.py
@@ -1,3 +1,4 @@
-__all__ = ("decode", "encode", "format", "parse")
+__all__ = ("encode", "ENCODE_DEFAULT_CHARS", "ENCODE_COMPONENT_CHARS")
 __version__ = "0.0.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
+from mdurl._encode import encode, ENCODE_DEFAULT_CHARS, ENCODE_COMPONENT_CHARS
---
+++ src/mdurl/_encode.py
@@ -0,0 +1,80 @@
+from string import ascii_letters, digits
+from urllib.parse import quote as encode_uri_component
+
+ASCII_LETTERS_AND_DIGITS = ascii_letters + digits
+
+ENCODE_DEFAULT_CHARS = ";/?:@&=+$,-_.!~*'()#"
+ENCODE_COMPONENT_CHARS = "-_.!~*'()"
+
+encode_cache = {}
+
+
+# Create a lookup array where anything but characters in `chars` string
+# and alphanumeric chars is percent-encoded.
+def get_encode_cache(exclude):
+    if exclude in encode_cache:
+        return encode_cache[exclude]
+
+    cache = []
+    encode_cache[exclude] = cache
+
+    for i in range(128):
+        ch = chr(i)
+
+        if ch in ASCII_LETTERS_AND_DIGITS:
+            # always allow unencoded alphanumeric characters
+            cache.append(ch)
+        else:
+            cache.append('%' + ('0' + hex(i)[2:].upper())[-2:])
+
+    for i in range(len(exclude)):
+        cache[ord(exclude[i])] = exclude[i]
+
+    return cache
+
+
+# Encode unsafe characters with percent-encoding, skipping already
+# encoded sequences.
+#
+#  - string       - string to encode
+#  - exclude      - list of characters to ignore (in addition to a-zA-Z0-9)
+#  - keepEscaped  - don't encode '%' in a correct escape sequence (default: true)
+def encode(string: str, exclude: str = ENCODE_DEFAULT_CHARS, *, keep_escaped: bool = True):
+    result = ""
+
+    cache = get_encode_cache(exclude)
+
+    l = len(string)
+    i = 0
+    while i < l:
+        code = ord(string[i])
+
+        #                              %
+        if keep_escaped and code == 0x25 and i+2<l:
+            if all(c in ASCII_LETTERS_AND_DIGITS for c in string[i+1:i+3]):
+                result += string[i:i+3]
+                i += 2
+                i += 1  # JS for loop statement3
+                continue
+
+        if code < 128:
+            result += cache[code]
+            i += 1  # JS for loop statement3
+            continue
+
+        if code >= 0xD800 and code <= 0xDFFF:
+            if code >= 0xD800 and code <= 0xDBFF and i + 1 < l:
+                next_code = ord(string[i+1])
+                if next_code >= 0xDC00 and next_code <= 0xDFFF:
+                    result += encode_uri_component(string[i] + string[i + 1])
+                    i += 1
+                    i += 1  # JS for loop statement3
+                    continue
+            result += '%EF%BF%BD'
+            i += 1  # JS for loop statement3
+            continue
+
+        result += encode_uri_component(string[i])
+        i += 1  # JS for loop statement3
+
+    return result
