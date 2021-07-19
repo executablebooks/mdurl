a7e6963ae1c71797d5267d50febd7b5f64515aea
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2021-07-19 23:43:37 +0300

    Port parse module

--- src/mdurl/__init__.py
+++ src/mdurl/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ("encode", "ENCODE_DEFAULT_CHARS", "ENCODE_COMPONENT_CHARS", "format")
+__all__ = ("encode", "ENCODE_DEFAULT_CHARS", "ENCODE_COMPONENT_CHARS", "format", "parse")
 __version__ = "0.0.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
 from mdurl._encode import encode, ENCODE_DEFAULT_CHARS, ENCODE_COMPONENT_CHARS
 from mdurl._format import format
+from mdurl._parse import url_parse as parse
--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,10 +1,26 @@
+import pytest
+
 from tests.fixtures.url import PARSED as FIXTURES
 
 from mdurl import parse
 
 
-def test_parse():
-    for url, expected in FIXTURES.items():
-        parsed = parse(url)
-        parsed = {k: v for k, v in parsed.items() if v is not None}
-        assert parsed == expected
+def is_url_and_dict_equal(url, url_dict):
+    return (url.protocol == url_dict.get("protocol")
+            and url.slashes == url_dict.get("slashes")
+            and url.auth == url_dict.get("auth")
+            and url.port == url_dict.get("port")
+            and url.hostname == url_dict.get("hostname")
+            and url.hash == url_dict.get("hash")
+            and url.search == url_dict.get("search")
+            and url.pathname == url_dict.get("pathname")
+            )
+
+
+@pytest.mark.parametrize(
+    "url,expected_dict",
+    FIXTURES.items(),
+)
+def test_parse(url, expected_dict):
+    parsed = parse(url)
+    assert is_url_and_dict_equal(parsed, expected_dict)
