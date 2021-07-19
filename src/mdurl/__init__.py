60b0dee5451f85ea18e2290579e1b6d0731b572a
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2021-07-20 02:24:39 +0300

    Port decode

--- src/mdurl/__init__.py
+++ src/mdurl/__init__.py
@@ -1,4 +1,7 @@
 __all__ = (
+    "decode",
+    "DECODE_DEFAULT_CHARS",
+    "DECODE_COMPONENT_CHARS",
     "encode",
     "ENCODE_DEFAULT_CHARS",
     "ENCODE_COMPONENT_CHARS",
@@ -7,6 +10,7 @@ __all__ = (
 )
 __version__ = "0.0.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
+from mdurl._decode import DECODE_COMPONENT_CHARS, DECODE_DEFAULT_CHARS, decode
 from mdurl._encode import ENCODE_COMPONENT_CHARS, ENCODE_DEFAULT_CHARS, encode
 from mdurl._format import format
 from mdurl._parse import url_parse as parse
