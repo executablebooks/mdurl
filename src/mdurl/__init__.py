e259d3cff6c7e9a52b6f55eb2412243d895a74a6
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2021-07-20 18:54:32 +0300

    Bump version: 0.0.0 → 0.0.1

--- src/mdurl/__init__.py
+++ src/mdurl/__init__.py
@@ -8,7 +8,7 @@ __all__ = (
     "format",
     "parse",
 )
-__version__ = "0.0.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+__version__ = "0.0.1"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
 from mdurl._decode import DECODE_COMPONENT_CHARS, DECODE_DEFAULT_CHARS, decode
 from mdurl._encode import ENCODE_COMPONENT_CHARS, ENCODE_DEFAULT_CHARS, encode
