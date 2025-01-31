7b7ae5d493089656393a0915958d38c8de953a7e
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2025-01-31 08:48:55 +0200

    Halve import time

--- src/mdurl/_url.py
+++ src/mdurl/_url.py
@@ -1,14 +1,32 @@
 from __future__ import annotations
 
-from typing import NamedTuple
+from collections import namedtuple
 
+TYPE_CHECKING = False
+if TYPE_CHECKING:
+    from typing import NamedTuple
 
-class URL(NamedTuple):
-    protocol: str | None
-    slashes: bool
-    auth: str | None
-    port: str | None
-    hostname: str | None
-    hash: str | None  # noqa: A003
-    search: str | None
-    pathname: str | None
+    class URL(NamedTuple):
+        protocol: str | None
+        slashes: bool
+        auth: str | None
+        port: str | None
+        hostname: str | None
+        hash: str | None  # noqa: A003
+        search: str | None
+        pathname: str | None
+
+else:
+    URL = namedtuple(
+        "URL",
+        [
+            "protocol",
+            "slashes",
+            "auth",
+            "port",
+            "hostname",
+            "hash",
+            "search",
+            "pathname",
+        ],
+    )
